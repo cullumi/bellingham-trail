dd92f0676b598a270fc16b30bb58ad963dcf67fd
ianpcullum <ianpcullum@penguin>
2020-07-17 04:33:27 -0700

    fixed broken printfs, fixed errors when setting DiNode values.  TODO: properly allocate memory to DiNode and its String (Useful link can be found in comment).

--- belTrail.c
+++ belTrail.c
@@ -2,30 +2,43 @@
 #include <stdio.h>
 #include <string.h>
 
-struct DiNode {
-  char* tag;
-  char* p;
-  char* o1; // Options
-  char* o2;
-  char* o3;
-  char* r1; // Responses
-  char* r2;
-  char* r3;
-  char* rd;
-  char* d1; // Destination Tags
-  char* d2;
-  char* d3;
-};
+typedef struct {
+  // char tag[12];
+  // char p[512];
+  // char o1[128]; // Options
+  // char o2[128];
+  // char o3[128];
+  // char r1[256]; // Responses
+  // char r2[256];
+  // char r3[256];
+  // char rd[256];
+  // char d1[12]; // Destination Tags
+  // char d2[12];
+  // char d3[12];
+  char *tag;
+  char *p;
+  char *o1; // Options
+  char *o2;
+  char *o3;
+  char *r1; // Responses
+  char *r2;
+  char *r3;
+  char *rd;
+  char *d1; // Destination Tags
+  char *d2;
+  char *d3;
+} DiNode;
 
-struct DiNode tree[4];
+DiNode tree[4];
 
-char* answerPrompt (struct DiNode prompt) {
+
+char* answerPrompt (DiNode prompt) {
 
   printf( "<> %s\n", prompt.p);
   printf( "  (1) %s\n  (2) %s\n  (3) %\n\n", prompt.o1, prompt.o2, prompt.o3);
 
   char* destination = NULL;
-  int answer;
+  int answer = 0;
   char* response = NULL;
   while (response == NULL){
     printf( "<> (1 , 2, or 3?):  ");
@@ -47,26 +60,35 @@ char* answerPrompt (struct DiNode prompt) {
         printf("\n<< %s\n\n", prompt.rd);
     }
   }
+  printf("%s\n", prompt.r1);
+  printf("%s\n", prompt.d1);
   printf("\n>> %s\n\n", response);
+  printf("%s\n", destination);
 
   return destination;
 }
 
+
 void breakPoint(){}
 
+// KEEP AN EYE OUT FOR BROKEN PRINTS-- i.e., always print strings using %s.
 void generateTree (FILE *fp) {
   //struct DiNode treeRoot[4];
-  struct DiNode node;
+  DiNode node; // Check this out later:  https://stackoverflow.com/questions/10162152/how-to-work-with-string-fields-in-a-c-struct
   int i = 1;
   int j = 0;
-  char line[512];
+  char line[512] = "Hello World My Name Is Ian, How Are You Doing Today?";
 
   while (fgets(line, sizeof line, fp) != NULL){
     if (strncmp(line,"\n",2)){
+      //printf("breakPoint");
+      // printf("%s\n", "Stuff");
+      printf("%s / %d\n", line, i);
+      // printf("%s\n", "Stuff");
       switch ( i % 13 ) {
-        case 1: node.tag = line; printf("%s\n", "SET TAG"); break;
-        case 2: node.p = line; printf("%s\n", "SET PROMPT"); break;
-        case 3: node.o1 = line; printf("%s\n", "SET OP 1"); break;
+        case 1: printf("%s / %s\n",node.tag, node.p); node.tag = line; printf("%s / %s\n",node.tag, node.p); break; //*strdup(line); printf("%s\n", "SET TAG"); break;
+        case 2: node.p = line; break; //printf("%s\n", "SET PROMPT"); break;
+        case 3: node.o1 = line; break; //printf("%s\n", "SET OP 1"); break;
         case 4: node.o2 = line; break;
         case 5: node.o3 = line; break;
         case 6: node.r1 = line; break;
@@ -76,15 +98,20 @@ void generateTree (FILE *fp) {
         case 10: node.d1 = line; break;
         case 11: node.d2 = line; break;
         case 12: node.d3 = line; break;
-        default : break;
+        default : printf("%s\n", "default switch"); break;
       }
       i++;
 
+      // printf("%s\n", "Stuff");
       breakPoint();
 
-      printf("%s ||| %s ||| %s", node.tag, node.p, node.d3);
-      printf("SWITCH");
-      printf("* %s", line);
+      // printf("%s\n", node.tag);
+      //printf("%s ||| %s ||| %s", node->tag, node->p, node->d3);
+      //printf("SWITCH");
+      printf("* %s\n", line);
+      //char new_line[512];
+      //*line = new_line;
+      //printf("* %s", line);
     }
     else {
       printf("NOT SWITCH");
@@ -92,7 +119,6 @@ void generateTree (FILE *fp) {
       tree[j] = node;
       //treeRoot[j] = node;
       j++;
-      i = 1;
     }
   }
 }
